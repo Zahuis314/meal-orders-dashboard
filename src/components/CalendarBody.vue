40da38e3fc9e61aa3fb17ab2bff3e97b3f6f58e2
Zahuis314 <zahuis314@gmail.com>
2020-10-15 13:48:41 -0400

    Add Weekdays to Calendar Body

--- src/components/CalendarBody.vue
+++ src/components/CalendarBody.vue
@@ -1,24 +1,49 @@
 <template>
-  <div id="calendar-body">
+	<div id="calendar-body" class="container">
 
-  </div>
+		<div class="weekdays">
+			<div class="weekday" v-for="weekday in weekdays" :key="weekday.format('[WeekDay:]d')">
+				<p>{{weekday.format('ddd')}}</p>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import CalendarDay from './CalendarDay.vue'
 
 export default {
-  name: 'CalendarBody',
-  components: {
-    CalendarDay
-  },
-  data() {
-    return {
-    }
-  },
+	name: 'CalendarBody',
+	components: {
+		CalendarDay
+	},
+	props: {
+		date: Object,
+	},
+	data() {
+		return {
+		}
+	},
+	computed:{
+		weekdays: function(){
+			var moment = this.moment
+			return Array.apply(null,Array(7)).map(function(_,i){
+				return moment(i,'e').startOf('week').isoWeekday(i+1)
+				// return moment().format('ddd')
+			})
+		}
+	}
 }
 </script>
 
 <style lang="scss">
-  
+	.weekdays
+	{
+		display:grid;
+		grid-template-columns: 14.2% 14.2% 14.2% 14.2% 14.2% 14.2% 14.2%;
+	}
+	.weekday{
+		// float: left;
+		text-align: center;
+	}
 </style>
